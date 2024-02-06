3b76c7526df34545d68ec07b6d2a7e3b6e8347c4
Amumux <fabriali4@gmail.com>
2024-02-06 12:53:04 +0100

    React-App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import Like from "./components/Like";
+import ExpandableTable from "./components/ExpandableTable";
 
 function App() {
   return (
     <div>
-      <Like></Like>
+      <ExpandableTable>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil quae
+        minima, officiis, quis nam commodi numquam, reiciendis molestiae tenetur
+        minus voluptate reprehenderit consequuntur? Dignissimos aperiam
+        aspernatur quas ratione iste voluptas blanditiis esse architecto eos
+        omnis. Unde quasi ducimus vel dicta commodi voluptates, dolor minus
+        provident sunt quia magni doloremque et tenetur labore deserunt
+        repudiandae cumque sequi neque tempore facere voluptatibus aut
+        repellendus tempora at. Omnis molestiae cupiditate fugit ratione eaque
+        natus maiores suscipit veniam pariatur blanditiis atque quis harum
+        possimus consectetur accusantium unde, quos earum, amet voluptatum eos.
+        Delectus dolores at vero ea nisi laudantium sit accusamus distinctio?
+        Nihil, animi?
+      </ExpandableTable>
     </div>
   );
 }
