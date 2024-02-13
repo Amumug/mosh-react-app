6a229b1cb322cb98326779928836238e2a0e1837
Amumux <fabriali4@gmail.com>
2024-02-13 12:39:36 +0100

    React-App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Form from "./components/Form";
 
+
 function App() {
   return (
     <div>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,22 @@
 import { FieldValues, useForm } from "react-hook-form";
-import { z } from "zod";
+import { object, z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 const schema = z.object({
-  name: z.string().min(3, {message: 'Name must be at least 3 characters'}),
-  age: z.number({invalid_type_error: 'Age field is required.'}).min(18, {message: 'Age must be at least 18'}),
+  name: z
+    .string()
+    .min(3, { message: "Description should be at least 3 characters" }),
+  age: z.number({ invalid_type_error: "Amount is required." }).min(1),
+  category: z.string({required_error: "Category is required."}).min(0, {message: ""})
 });
 
 type FormData = z.infer<typeof schema>;
 
-const Form = () => {
+  const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => console.log(data);
@@ -22,7 +25,7 @@ const Form = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="from-label">
-          Name
+          Description
         </label>
         <input
           {...register("name")}
@@ -30,25 +33,39 @@ const Form = () => {
           type="text"
           className="form-control"
         />
-        {errors.name && (
-        <p className="text-danger">{errors.name.message}</p>
-        )}
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="from-label">
-          Age
+          Amount
         </label>
         <input
-          {...register("age", {valueAsNumber: true})}
+          {...register("age", { valueAsNumber: true })}
           id="age"
           type="number"
           className="form-control"
         />
-         {errors.age && (
-        <p className="text-danger">{errors.age.message}</p>
-        )}
+        {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
-      <button className="btn btn-primary">Submit</button>
+      <div className="mb-3" >
+        <label htmlFor="category" className="from-label">
+          Category
+        </label>
+        <select
+          id="category"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 form-control"
+          // {...register("category")}
+        >
+          <option selected></option>
+          <option value="US">Groceries</option>
+          <option value="CA">Utilities</option>
+          <option value="FR">Entertaiment</option>
+        </select>
+        {errors.category && <p className="text-danger">{errors.category.message}</p>}
+      </div>
+      <button  className="btn btn-primary">
+        Submit
+      </button>
     </form>
   );
 };
