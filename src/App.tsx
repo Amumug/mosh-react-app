3c2244949d28714d4441650952070ece511cbd24
Amumux <fabriali4@gmail.com>
2024-02-19 12:54:20 +0100

    React App

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,30 @@ import { useState } from "react";
 import ExpenseList from "./components/expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./components/expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./components/expense-tracker/components/ExpenseForm";
-
-export const categories = ['Groceries', 'Utilities', 'Entertaiment']
+import categories from "./components/expense-tracker/categories";
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState('')
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aa", amount: 12, category: "Utilities" },
     { id: 3, description: "aac", amount: 15, category: "Entertainment" },
     { id: 2, description: "aab", amount: 14, category: "Groceries" },
-
   ]);
 
-  const visibleExpenses = selectedCategory ? expenses.filter((e) => e.category === selectedCategory) : expenses
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
 
   return (
-
     <div>
       <div className="mb-5">
-            <ExpenseForm/>
-        </div>
-    <div className="mb-3">
-      <ExpenseFilter onSelectCategory={category => setSelectedCategory(category)}/>
-    </div>
+        <ExpenseForm onSubmit={expense => setExpenses([...expenses, { ...expense, id: expenses.length + 1}])}/>
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
       <ExpenseList
         expenses={visibleExpenses}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
--- src/components/expense-tracker/components/ExpenseList.tsx
+++ src/components/expense-tracker/components/ExpenseList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import categories from "../categories";
 
 interface Expense {
   id: number;
