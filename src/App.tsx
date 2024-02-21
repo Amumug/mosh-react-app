145592ddaff9cbe67e9fd7fa2bbb9e0e6b7e0157
Amumux <fabriali4@gmail.com>
2024-02-21 22:10:19 +0100

    React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,21 @@
-import { useState } from "react";
-import ExpenseList from "./components/expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./components/expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./components/expense-tracker/components/ExpenseForm";
-import categories from "./components/expense-tracker/categories";
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+interface User{
+  id: number;
+  name: string;
+}
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "aa", amount: 12, category: "Utilities" },
-    { id: 3, description: "aac", amount: 15, category: "Entertainment" },
-    { id: 2, description: "aab", amount: 14, category: "Groceries" },
-  ]);
-
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
-
+  const [users, setUsers] = useState<User[]>([])
+  useEffect (() => {
+    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .then(res => setUsers(res.data))
+  }, [])
   return (
-    <div>
-      <div className="mb-5">
-        <ExpenseForm onSubmit={expense => setExpenses([...expenses, { ...expense, id: expenses.length + 1}])}/>
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-      />
-    </div>
+    <ul>
+      {users.map(user => <li key={user.id}>{user.name}</li>)}
+    </ul>
   );
 }
 
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const ProductList = () => {
+  const [users, setUsers] = useState([])
+  useEffect (() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(res => console.log(res))
+  })
   return (
-    <div>ProductList</div>
+    <div></div>
   )
 }
 
