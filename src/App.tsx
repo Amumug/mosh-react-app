9f43dc221028ab43041f33b89222df9fb1df49ce
Amumux <fabriali4@gmail.com>
2024-02-22 18:20:15 +0100

    React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,56 @@
-import { useEffect, useState } from 'react'
-import axios, { CanceledError } from 'axios'
+import { useEffect, useState } from "react";
+import axios, { CanceledError } from "axios";
 
-interface User{
+interface User {
   id: number;
   name: string;
 }
 
 function App() {
-  const [users, setUsers] = useState<User[]>([])
-  const [error, setError] = useState("")
-  const [isLoading, setLoading] = useState(false)
-  useEffect (() => {
-    const controller = new AbortController()
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+  useEffect(() => {
+    const controller = new AbortController();
 
-    setLoading(true)
-    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
-      .then(res => {
-        setUsers(res.data)
-        setLoading(false)
+    setLoading(true);
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
       })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
-        setLoading(false)
-      })
+        setLoading(false);
+      });
 
-      return () => controller.abort()
-  }, [])
+    return () => controller.abort();
+  }, []);
+
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users]
+    setUsers(users.filter(u => u.id !== user.id))
+
+    axios.delete('https://jsonplaceholder.typicode.com/xusers/' + user.id)
+     .catch(err => {
+      setError(err.message)
+      setUsers(originalUsers)
+     })
+  }
   return (
     <>
-    {error && <p className='text-danger'>{error}</p>}
-    {isLoading && <div className="spinner-border"></div>}
-    <ul>
-      {users.map(user => <li key={user.id}>{user.name}</li>)}
-    </ul>
+      {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+      <ul className="list-group">
+        {users.map((user) => (
+          <li key={user.id} className="list-group-item d-flex justify-content-between">
+            {user.name}
+            <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
