5f604dd98bab5e75ca8b9af46f38f9342e2125b2
Amumux <fabriali4@gmail.com>
2024-02-22 13:25:11 +0100

    React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios'
+import axios, { CanceledError } from 'axios'
 
 interface User{
   id: number;
@@ -8,14 +8,33 @@ interface User{
 
 function App() {
   const [users, setUsers] = useState<User[]>([])
+  const [error, setError] = useState("")
+  const [isLoading, setLoading] = useState(false)
   useEffect (() => {
+    const controller = new AbortController()
+
+    setLoading(true)
     axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
-      .then(res => setUsers(res.data))
+      .then(res => {
+        setUsers(res.data)
+        setLoading(false)
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false)
+      })
+
+      return () => controller.abort()
   }, [])
   return (
+    <>
+    {error && <p className='text-danger'>{error}</p>}
+    {isLoading && <div className="spinner-border"></div>}
     <ul>
       {users.map(user => <li key={user.id}>{user.name}</li>)}
     </ul>
+    </>
   );
 }
 
